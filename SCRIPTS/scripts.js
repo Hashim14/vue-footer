6afa0a91be95043df4e512c95a8e4f8adf6877da
Hashim14 <45725764+Hashim14@users.noreply.github.com>
2019-04-23 18:28:16 +0530

    Add files via upload

---
+++ SCRIPTS/scripts.js
@@ -0,0 +1,13 @@
+new Vue({
+  el: '#simpleApp',
+  data: () => ({
+    icons: [
+      'fab fa-facebook',
+      'fab fa-twitter',
+      'fab fa-google-plus',
+      'fab fa-linkedin',
+      //'fab fa-instagram'
+      'fab fa-apple'
+    ]
+  })
+})
